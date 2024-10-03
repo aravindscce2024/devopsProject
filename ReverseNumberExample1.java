922ddccca6c4dd1d2315aaabec4d4d41caa35367
johnny <aravindkumarn@scce.ac.in>
2024-10-03 10:39:15 +0530

    made changes to string

--- ReverseNumberExample1.java
+++ ReverseNumberExample1.java
@@ -2,7 +2,7 @@ public class ReverseNumberExample1
 {  
 public static void main(String[] args)   
 {  
-int number = 98765, reverse = 0;  
+int number = 987652, reverse = 0;  
 while(number != 0)   
 {  
 int remainder = number % 10;  

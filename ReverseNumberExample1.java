4cf47a218a96d4e48b613324bfbb1306a267b6e3
johnny <aravindkumarn@scce.ac.in>
2024-11-13 12:42:53 +0530

    new input variable

--- ReverseNumberExample1.java
+++ ReverseNumberExample1.java
@@ -2,7 +2,7 @@ public class ReverseNumberExample1
 {  
 public static void main(String[] args)   
 {  
-int number = 789899, reverse = 0;  
+int number = 675658, reverse = 0;  
 while(number != 0)   
 {  
 int remainder = number % 10;  
